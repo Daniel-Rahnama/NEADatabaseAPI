bdb9925fd4f0c9585e9971e5c589b7242975d876
Daniel Rahnama <danielrahnama@outlook.com>
2023-08-24 23:28:21 +0100

    Implemented addLeaderboard

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type LeaderboardPlayer struct {
@@ -19,49 +21,68 @@ type LeaderboardData struct {
 }
 
 func getLeaderboard(c echo.Context, db *sql.DB) error {
-	query, err := db.Query(`SELECT * FROM leaderboard`);
+	query, err := db.Query(`SELECT * FROM leaderboard`)
 
-	if (err != nil) {
-		panic(err.Error);
+	if err != nil {
+		panic(err.Error)
 	}
 
-	defer query.Close();
+	defer query.Close()
 
-	data := LeaderboardData{Leaderboard:map[string]LeaderboardPlayer{}};
+	data := LeaderboardData{Leaderboard: map[string]LeaderboardPlayer{}}
 
-	for (query.Next()) {
-		var name string;
+	for query.Next() {
+		var name string
 
-		var time1, time2, time3 int;
+		var time1, time2, time3 int
 
-		err := query.Scan(&name, &time1, &time2, &time3);
+		err := query.Scan(&name, &time1, &time2, &time3)
 
-		if (err != nil) {
-			panic(err.Error);
+		if err != nil {
+			panic(err.Error)
 		}
 
-		data.Leaderboard[name] = LeaderboardPlayer{Time1:time1, Time2:time2, Time3:time3};
+		data.Leaderboard[name] = LeaderboardPlayer{Time1: time1, Time2: time2, Time3: time3}
 	}
 
-	return c.JSONPretty(http.StatusOK, data, "  ");
+	return c.JSON(http.StatusOK, data)
+}
+
+func addLeaderboard(c echo.Context, db *sql.DB) error {
+	data := new(LeaderboardData)
+	c.Bind(data)
+
+	for name, times := range data.Leaderboard {
+		_, err := db.Exec(`INSERT INTO leaderboard VALUES("` + name + `", ` + strconv.Itoa(times.Time1) + `, ` + strconv.Itoa(times.Time2) + `, ` + strconv.Itoa(times.Time3) + `)`)
+
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	return c.NoContent(http.StatusOK)
 }
 
 func main() {
-	e := echo.New();
+	e := echo.New()
 
-	e.Use(middleware.CORS());
+	e.Use(middleware.CORS())
 
-	db, err := sql.Open("mysql", "root:admin@tcp(localhost:3306)/NEAGameDB");
+	db, err := sql.Open("mysql", "root:admin@tcp(localhost:3306)/NEAGameDB")
 
-	if (err != nil) {
-		panic(err.Error);
+	if err != nil {
+		panic(err.Error)
 	}
 
-	defer db.Close();
+	defer db.Close()
 
 	e.GET("/leaderboard", func(c echo.Context) error {
-		return getLeaderboard(c, db);
-	});
+		return getLeaderboard(c, db)
+	})
 
-	e.Logger.Fatal(e.Start(":1323"));
+	e.POST("/leaderboard", func(c echo.Context) error {
+		return addLeaderboard(c, db)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
