775d273a5e7687e57729a4f5902a1ef95462ffd2
Daniel-Rahnama <36826613+Daniel-Rahnama@users.noreply.github.com>
2023-09-09 11:10:52 +0100

    Format CreateDBQuery.sql

--- Queries/CreateDBQuery.sql
+++ Queries/CreateDBQuery.sql
@@ -2,8 +2,8 @@ CREATE DATABASE NEAGameDB;
 
 CREATE TABLE Leaderboard (
 	Username varchar(255) NOT NULL,
-    Time1 int,
-    Time2 int,
+	Time1 int,
+	Time2 int,
     Time3 int,
-    PRIMARY KEY (Username)
+	PRIMARY KEY (Username)
 );
